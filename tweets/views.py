4174457c2655258f09aeff791331e355a4527d18
kawaharm <mjbk295@gmail.com>
2022-02-07 14:23:55 -0800

    added print logs to test get_tweets response data

--- tweets/views.py
+++ tweets/views.py
@@ -46,6 +46,7 @@ def get_tweets(request):
     if request.method == 'POST':
         # Decode json
         name = request.body.decode('utf-8')
+        print('REQUEST BODY FROM FRONTEND: ', name)
         q = []
         for stock in stock_list:
             if stock.get('name') == name:
@@ -53,6 +54,7 @@ def get_tweets(request):
         url = BASE_URL+"?query={}%20%23{}&max_results=100&sort_order=recency&tweet.fields=created_at".format(
             q[0], q[1])
         response = execute_twitter_api_call(url)
+        print('REPONSE FROM TWITTER API: ', response)
 
         '''
         Sentiment Analysis using VADER lexicon.
