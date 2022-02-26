fc66d0016cced3d1d48a040534be2abfc849d5f2
kawaharm <mjbk295@gmail.com>
2022-02-25 16:09:27 -0800

    updated stock views.py

--- stocks/views.py
+++ stocks/views.py
@@ -9,38 +9,10 @@ import finnhub
 API_KEY = settings.FINNHUB_API_KEY
 
 
-stock_list = [
-    {
-        "name": "gme",
-        "keywords": ["gamestop", "gme"],
-    },
-    {
-        "name": "aapl",
-        "keywords": ["apple", "aapl"],
-    },
-]
-
-
 @csrf_exempt
 def get_stocks(request):
 
     if request.method == 'POST':
-        # # Decode bytestring
-        # req = request.body.decode('utf-8')
-        # # Convert bytestring to dictionary
-        # stock = ast.literal_eval(req)
-        # print("stock info from REACT: ", stock)
-
-        # BASE_URL = "https://api.polygon.io/v2/aggs/"
-        # url = BASE_URL+"ticker/{}/range/1/day/{}/{}".format(
-        #     stock["ticker"], stock["dates"][0], stock["dates"][1])
-        # print('URL', url)
-        # response = execute_polygon_api_call(url)
-        # print('response data', response)
-        # print('response type', type(response))
-
-        # return HttpResponse(json.dumps(response))
-
         # Decode bytestring
         req = request.body.decode('utf-8')
         # Convert bytestring to dictionary
