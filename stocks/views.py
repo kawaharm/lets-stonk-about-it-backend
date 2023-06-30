c033b24f514b0eb6a595529c62890eb9ab05292f
kawaharm <mjbk295@gmail.com>
2023-06-29 18:15:26 -0700

    switched from matlib to chart.js to render graph

--- stocks/views.py
+++ stocks/views.py
@@ -8,6 +8,8 @@ from matplotlib import pyplot as plt
 import base64
 from io import BytesIO
 import yfinance as yf
+import json
+from datetime import datetime
 
 
 @csrf_exempt
@@ -27,27 +29,21 @@ def get_stocks(request):
         stock_data = yf.download(
             company, period=period, interval=interval)
 
-        def create_graph():
-            # Create buffer for saving image of graph
-            buffer = BytesIO()
-            plt.savefig(buffer, format='png')
-            buffer.seek(0)
-            # Encode image then decode to utf-8
-            encoded = base64.b64encode(buffer.getvalue()).decode('utf-8')
-            buffer.close()  # free buffer memory
-            return encoded
+        def generate_graph_data(stock_data, interval):
+            # Retrieve closing prices in JSON string
+            data_json = stock_data['Close'].to_json(
+                orient='split')
+            # Convert JSON to dict
+            data_dict = json.loads(data_json)
+            dates = data_dict['index']
+            for i in range(len(dates)):
+                # Convert epoch time to datetime object
+                date_obj = datetime.fromtimestamp(dates[i]//1000)
+                if interval == '60m':
+                    dates[i] = date_obj.strftime("%H:%M %p")
+                else:
+                    dates[i] = date_obj.strftime("%Y-%m-%d")
+            # Revert back to JSON string
+            return json.dumps(data_dict)
 
-        plt.switch_backend('AGG')
-        close = stock_data['Close']  # Extract closing prices
-        fig, ax = plt.subplots()
-        ax.plot(close, color='red')
-        plt.title('Stock Performance for ${}'.format(company))
-        plt.xlabel('Date')
-        plt.xticks(rotation=45)
-        plt.ylabel('Closing Price (USD)')
-        plt.tight_layout()
-        plotfinal = create_graph()
-        html_graph = 'data:image/png;base64, {}'.format(
-            plotfinal)
-
-        return HttpResponse(html_graph)
+        return HttpResponse(generate_graph_data(stock_data, interval))
